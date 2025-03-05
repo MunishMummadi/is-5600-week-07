8e1000cd782f89b6f5a5588190c2df4e9e00a3dc
Munish <moneymindedmunish1@gmail.com>
2025-03-04 19:23:38 -0600

    chore: changes

--- src/state/CartProvider.jsx
+++ src/state/CartProvider.jsx
@@ -1,20 +1,16 @@
 import React, { useReducer, useContext } from 'react'
 
-// Initialize the context
 const CartContext = React.createContext()
 
-// Definte the default state
 const initialState = {
   itemsById: {},
   allItems: [],
 }
 
-// Define reducer actions
 const ADD_ITEM = 'ADD_ITEM'
 const REMOVE_ITEM = 'REMOVE_ITEM'
 const UPDATE_ITEM_QUANTITY = 'UPDATE_ITEM_QUANTITY'
 
-// Define the reducer
 const cartReducer = (state, action) => {
   const { payload } = action;
   switch (action.type) {
@@ -31,7 +27,6 @@ const cartReducer = (state, action) => {
               : 1,
           },
         },
-        // Use `Set` to remove all duplicates
         allItems: Array.from(new Set([...state.allItems, action.payload._id])),
       };
       return newState
@@ -49,13 +44,27 @@ const cartReducer = (state, action) => {
         ),
       }
       return updatedState
+      case UPDATE_ITEM_QUANTITY:
+        console.log({state});
+        const currentItem = state.itemsById[payload._id]
+        const updateItemState = {
+          ...state,
+          itemsById:{
+            ...state.itemsById,
+          [payload._id]: {
+            ...currentItem,
+            quantity : currentItem.quantity + payload.quantity,
+          },
+        }
+      }
+
+        return updateItemState;
     
     default:
       return state
   }
 }
 
-// Define the provider
 const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initialState)
 
@@ -64,19 +73,16 @@ const CartProvider = ({ children }) => {
     dispatch({ type: REMOVE_ITEM, payload: product })
   }
 
-  // Add an item to the cart
   const addToCart = (product) => {
     dispatch({ type: ADD_ITEM, payload: product })
   }
 
-  // todo Update the quantity of an item in the cart
   const updateItemQuantity = (productId, quantity) => {
-    // todo
+    dispatch({ type: UPDATE_ITEM_QUANTITY, payload: {_id: productId , quantity}})
   }
 
-  // todo Get the total price of all items in the cart
   const getCartTotal = () => {
-    // todo
+    return getCartItems().reduce((acc,item) => acc + item.price * item.quantity, 0);
   }
 
   const getCartItems = () => {
